fa8452d4dce5601ac2b6504593d2bae0022123d7
diMo2004 <143714128+diMo2004@users.noreply.github.com>
2024-04-06 15:01:55 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class diMo
+{
+  public static void main(String []args)
+  {
+    double a,b;
+    Scanner sc=new Scanner(System.in);
+    a=sc.nextDouble();
+    b=sc.nextDouble();
+    double c=a*b;
+    double d=a/b;
+    double e=a+b;
+    double f=a-b;
+    double g=a%b;
+    System.out.println(c+"\n"+d+"\n"+e+"\n"+f+"\n"+g);
+  }
+}
